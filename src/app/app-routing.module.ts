0ef06d9cf5d70f9b12dc9be60d947f5762e0a47a
~ <you@example.com>
2023-07-13 14:13:27 +0530

    kare

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KannadaComponent } from './kannada/kannada.component';
+import { EnglishComponent } from './english/english.component';
+import { SingerComponent } from './singer/singer.component';
+import { TypesComponent } from './types/types.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'kannada',component:KannadaComponent},
+  {path:'english',component:EnglishComponent},
+  {path:'singer',component:SingerComponent},
+  {path:'types',component:TypesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
+
 import { SingerComponent } from './singer/singer.component';
 import { TypesComponent } from './types/types.component';
-=======
+
 import { KannadaComponent } from './kannada/kannada.component';
 import { EnglishComponent } from './english/english.component';
->>>>>>> 413a47eccdc42e6227ee101b1e4bdf27cadfd714
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-<<<<<<< HEAD
+
     SingerComponent,
-    TypesComponent
-=======
+    TypesComponent,
+
     KannadaComponent,
     EnglishComponent
->>>>>>> 413a47eccdc42e6227ee101b1e4bdf27cadfd714
+
   ],
   imports: [
     BrowserModule,
